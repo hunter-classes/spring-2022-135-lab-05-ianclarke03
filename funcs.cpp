bc19930c07ec1b9d7e793d876b30614368631cc5
ianclarke03 <83726782+ianclarke03@users.noreply.github.com>
2022-03-02 22:49:29 +0000

    d almost work

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "funcs.h"
+//#include "funcs.h"
 
 // add functions here
 
@@ -28,38 +28,64 @@ bool isPrime(int n) {
 /*
 int nextPrime(int n)
 {
-  //int num = n + 1;
+  int result;
   for (int i = 2; i <= n-1; i++)
   {
-    n++;
+    //n++;
     int remainder = n % i;
-    if (remainder == 0) // is not a prime
+    if (remainder != 0) // is a prime
+      result = n;
+    else if (remainder == 0)
       n++;
-    else if (remainder != 0)
-      return n;
     }
+  return result;
 }
 */
 
-/*
+int countPrimes(int a, int b)
+{
+  //for(int q; a <= q <= b; q++)
+  while (a <= b)
+    {
+      int x = 0;
+      for (int i = 2; i <= a-1; i++)
+      {
+        int remainder = a % i;
+        if (remainder != 0) // is a prime
+        x++;
+      }
+      if (x == (a-2) && (a < b))
+      {
+        std::cout << a << ", ";
+      }
+      a++;
+    }
+}
+
+
+
 int main()
 {
-  std::cout << "Task A\n";
+  std::cout << "Task A:\n";
   std::cout << isDivisibleBy(10,5) << "\n"; //test 10,5 and 11,4
   std::cout << isDivisibleBy(11,4) << "\n";
 
-  std::cout << "Task B\n";
+  std::cout << "Task B:\n";
   std::cout << isPrime(8) << "\n"; // test 2,8,15,17
   std::cout << isPrime(15) << "\n";
   std::cout << isPrime(17) << "\n";
   std::cout << isPrime(2) << "\n";
-
-  std::cout << "Task C\n";
+/*
+  std::cout << "Task C:\n";
   
   std::cout << nextPrime(2) << "\n"; // test 2, 14, and 17
   std::cout << nextPrime(14) << "\n";
   std::cout << nextPrime(17) << "\n";
+*/
 
+  std::cout << "Task D:\n";
+  std::cout << countPrimes(7, 21) <<"\n";
+
+  
   return 0;
 }
-*/
