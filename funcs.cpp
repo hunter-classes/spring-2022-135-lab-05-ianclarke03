8d8def3c622908627a4b8c90c48fa53310d3376a
ianclarke03 <83726782+ianclarke03@users.noreply.github.com>
2022-03-02 03:36:57 +0000

    c

--- funcs.cpp
+++ funcs.cpp
@@ -43,13 +43,17 @@ int nextPrime(int n)
 
 int main()
 {
+  std::cout << "Task A\n";
   std::cout << isDivisibleBy(10,5) << "\n"; //test 10,5 and 11,4
-  
-  std::cout << isPrime(8) << "\n"; // test 2, 15, and 23
+  std::cout << isDivisibleBy(11,4) << "\n";
+
+  std::cout << "Task B\n";
+  std::cout << isPrime(8) << "\n"; // test 2,8,15,17
   std::cout << isPrime(15) << "\n";
   std::cout << isPrime(17) << "\n";
   std::cout << isPrime(2) << "\n";
 
+  std::cout << "Task C\n";
   /*
   std::cout << nextPrime(2) << "\n"; // test 2, 14, and 17
   std::cout << nextPrime(14) << "\n";
