9ef3ddc462587d82d7996e8dc7f32b7d29d175fd
ianclarke03 <83726782+ianclarke03@users.noreply.github.com>
2022-03-02 01:42:37 +0000

    a and b

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
-#include "funcs.h"
+//#include "funcs.h"
 
 // add functions here
 
+bool isDivisibleBy(int n, int d)
+{
+    int remainder = n % d;
+
+    if(remainder == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool isPrime(int n)
+{
+  for (int i = 2; i <= n-1; i++)
+  {
+    if (n % i != 0)
+      return false;
+    else if (n % i == 0)
+      return true;
+  }
+  std::cout << "\n";
+}
+
+
+int main()
+{
+  std::cout << isDivisibleBy(10,5) << "\n";
+  std::cout << isPrime (10) << "\n";
+
+  return 0;
+}
