d898769ffc53534dfbb4e01164ff99507f4bfd54
ianclarke03 <83726782+ianclarke03@users.noreply.github.com>
2022-03-03 02:01:29 +0000

    dfgfg

--- funcs.cpp
+++ funcs.cpp
@@ -19,50 +19,78 @@ bool isPrime(int n) {
   for (int i = 2; i <= n-1; i++)
   {
     int remainder = n % i;
-    if (remainder != 0) // is a prime
+    if (remainder != 0)
       x++;
     }
-  return x == (n-2);
+  return x == (n-2); // prime
 }
 
 /*
 int nextPrime(int n)
 {
-  int result;
-  for (int i = 2; i <= n-1; i++)
-  {
-    //n++;
-    int remainder = n % i;
-    if (remainder != 0) // is a prime
-      result = n;
-    else if (remainder == 0)
-      n++;
-    }
-  return result;
+  while !(isPrime(int n))
+{
+n++;
+if isPrime(int n)
+return n;
 }
+  }
 */
 
+//add 1 to inout, if that number isnt a prime, add one to number, if it is, print that number.
+int nextPrime(int n)
+{
+  n++;
+  int nextprime = 0;
+  while (nextprime = 0)
+  {
+  int x = 0;
+  for (int i = 2; i <= n-1; i++)
+    {
+    int remainder = n % i;
+    if (remainder != 0)
+      x++;
+    }
+  if (x == (n-2)) //prime
+    nextprime = n;
+  else if (x != (n-2))
+    n++;
+  }
+  return nextprime;
+}
+
+/*
+//doesnt work for single digit primes
+int nextPrime(int n) {
+	n++;
+	while (n % 2 == 0 || n % 3 == 0 || n % 4 == 0 || n % 5 == 0 || n % 6 == 0 || n % 7 == 0 || n % 8 == 0 || n % 9 == 0) {
+		n++;
+	}
+	return n;
+*/
+
+/*
 int countPrimes(int a, int b)
 {
-  //for(int q; a <= q <= b; q++)
-  while (a <= b)
+  int place = a;
+  while (a <= place <= b)
     {
       int x = 0;
-      for (int i = 2; i <= a-1; i++)
+      for (int i = 2; i <= place-1; i++)
       {
-        int remainder = a % i;
+        int remainder = place % i;
         if (remainder != 0) // is a prime
         x++;
       }
-      if (x == (a-2) && (a < b))
+      if (x == (place-2) )
       {
-        std::cout << a << ", ";
+        std::cout << place << ", ";
       }
-      a++;
+      place++;
     }
 }
-
-
+*/
+//&& (a < b)
 
 int main()
 {
@@ -75,17 +103,18 @@ int main()
   std::cout << isPrime(15) << "\n";
   std::cout << isPrime(17) << "\n";
   std::cout << isPrime(2) << "\n";
-/*
+
   std::cout << "Task C:\n";
   
   std::cout << nextPrime(2) << "\n"; // test 2, 14, and 17
+  std::cout << nextPrime(3) << "\n";
   std::cout << nextPrime(14) << "\n";
   std::cout << nextPrime(17) << "\n";
-*/
 
+/*
   std::cout << "Task D:\n";
-  std::cout << countPrimes(7, 21) <<"\n";
-
+  std::cout << countPrimes(2, 7) <<"\n";
+*/
   
   return 0;
 }
